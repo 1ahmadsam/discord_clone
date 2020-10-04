81b2927560129c5bf5cf1be4ecab32ac3ae7806c
1ahmadsam <samiahmad360@hotmail.com>
2020-10-04 00:53:11 -0400

    added db to project

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,48 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const Message = require('./models/message');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// API routes
+app.get('/', (req, res) => res.status(200).send('hello world'));
+
+app.post('/messages', async (req, res) => {
+  const body = req.body;
+  const message = new Message({
+    message: body.message,
+    date: Date.now(),
+  });
+
+  if (message.message) {
+    const savedMessage = await message.save();
+    res.status(201).json(savedMessage.toJSON());
+  } else {
+    return res.status(401).json({ error: 'message was not sent' });
+  }
+});
+// Listen command
+exports.api = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
---
+++ functions/models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema({
+  message: String,
+  date: Date,
+});
+
+messageSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Message', messageSchema);
